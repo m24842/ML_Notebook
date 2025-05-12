c97e641676c8218761612e2de3a8a87cd02afe2e
m24842 <138349218+m24842@users.noreply.github.com>
2025-05-11 20:15:21 -0400

    save

--- src/Packages/m24842_ml/ml/models/pc/pc.py
+++ src/Packages/m24842_ml/ml/models/pc/pc.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from einops import rearrange, repeat
-from contextlib import contextmanager
+from contextlib import nullcontext, contextmanager
 
 @contextmanager
 def no_param_grad(model):
@@ -63,6 +63,7 @@ class PCModel(nn.Module):
         self.device = device
         self.to(device)
     
+    @torch.no_grad()
     def get_io_shapes(self, x=None, y=None):
         if x is not None:
             for layer in self.layers:
@@ -73,6 +74,7 @@ class PCModel(nn.Module):
                 y = layer.get_io_shapes(None, y)
             return y.shape
     
+    @torch.no_grad()
     def update_io_shapes(self, x=None, y=None):
         if (x is not None and self.cached_x_shape == x.shape) or (y is not None and self.cached_y_shape == y.shape): return
         if x is not None:
@@ -115,7 +117,7 @@ class PCModel(nn.Module):
     def train_forward(self, x, y, param_optimizer, param_grad_clip_norm=None, iterative=False, init_dir="forward"):
         self.update_io_shapes(x, y)
         
-        # Training
+        # Amortized state initialization
         if init_dir == "forward":
             state_tensors = self.forwad_state_init(x)
             state_tensors[-1] = y
@@ -160,30 +162,31 @@ class PCModel(nn.Module):
         
         self.update_io_shapes(x, y)
 
-        if x is not None:
-            # Forward inference
-            state_tensors = self.forwad_state_init(x)
-            energy_optimizer = self.energy_optimizer_class(state_tensors[1:], lr=self.energy_lr) # Exclude input
-            for i in range(self.max_its):
-                energy_optimizer.zero_grad()
-                energy = self.compute_model_energy(state_tensors)
-                energy.backward()
-                if self.energy_grad_clip_norm is not None: torch.nn.utils.clip_grad_norm_(state_tensors, max_norm=self.energy_grad_clip_norm)
-                energy_optimizer.step()
-                if energy.item() < self.min_energy: break
-            return state_tensors[-1]
-        elif y is not None:
-            # Backward inference
-            state_tensors = self.backward_state_init(y)
-            energy_optimizer = self.energy_optimizer_class(state_tensors[:-1], lr=self.energy_lr) # Exclude output
-            for i in range(self.max_its):
-                energy_optimizer.zero_grad()
-                energy = self.compute_model_energy(state_tensors)
-                energy.backward()
-                if self.energy_grad_clip_norm is not None: torch.nn.utils.clip_grad_norm_(state_tensors, max_norm=self.energy_grad_clip_norm)
-                energy_optimizer.step()
-                if energy.item() < self.min_energy: break
-            return state_tensors[0]
+        with nullcontext() if self.training else no_param_grad(self):
+            if x is not None:
+                # Forward inference
+                state_tensors = self.forwad_state_init(x)
+                energy_optimizer = self.energy_optimizer_class(state_tensors[1:], lr=self.energy_lr) # Exclude input
+                for i in range(self.max_its):
+                    energy_optimizer.zero_grad()
+                    energy = self.compute_model_energy(state_tensors)
+                    energy.backward()
+                    if self.energy_grad_clip_norm is not None: torch.nn.utils.clip_grad_norm_(state_tensors, max_norm=self.energy_grad_clip_norm)
+                    energy_optimizer.step()
+                    if energy.item() < self.min_energy: break
+                return state_tensors[-1]
+            elif y is not None:
+                # Backward inference
+                state_tensors = self.backward_state_init(y)
+                energy_optimizer = self.energy_optimizer_class(state_tensors[:-1], lr=self.energy_lr) # Exclude output
+                for i in range(self.max_its):
+                    energy_optimizer.zero_grad()
+                    energy = self.compute_model_energy(state_tensors)
+                    energy.backward()
+                    if self.energy_grad_clip_norm is not None: torch.nn.utils.clip_grad_norm_(state_tensors, max_norm=self.energy_grad_clip_norm)
+                    energy_optimizer.step()
+                    if energy.item() < self.min_energy: break
+                return state_tensors[0]
 
 # from torchvision import datasets, transforms
 # from torch.utils.data import DataLoader
@@ -198,12 +201,12 @@ class PCModel(nn.Module):
 
 # model = PCModel(
 #     layers=nn.ModuleList([
-#         PCLayer(nn.Sequential(nn.Linear(784, 128, bias=False), nn.ReLU(), nn.Linear(128, 128, bias=False)), nn.Sequential(nn.Linear(128, 128, bias=False), nn.ReLU(), nn.Linear(128, 784, bias=False)), device=device),
+#         PCLayer(nn.Sequential(nn.Linear(784, 128, bias=False), nn.ReLU()), nn.Sequential(nn.Linear(128, 784, bias=False), nn.ReLU()), device=device),
 #         PCLayer(nn.Linear(128, 10, bias=False), nn.Linear(10, 128, bias=False), f_loss_fn=F.cross_entropy, b_loss_fn=F.mse_loss, device=device)
 #     ]),
 #     max_its=2,
 #     min_energy=1e-3,
-#     energy_lr=1e-3,
+#     energy_lr=1e-0,
 #     energy_optimizer_class=optim.SGD,
 #     device=device
 # )
@@ -226,16 +229,15 @@ class PCModel(nn.Module):
 
 # def test():
 #     model.eval()
-#     with no_param_grad(model):
-#         total_loss = 0
-#         correct = 0
-#         for data, target in test_loader:
-#             data = data.flatten(1).to(device)
-#             target = target.to(device)
-#             y = model(data)
-#             loss = F.cross_entropy(y, target)
-#             total_loss += loss.item()
-#             correct += (y.argmax(dim=1) == target).sum().item()
+#     total_loss = 0
+#     correct = 0
+#     for data, target in test_loader:
+#         data = data.flatten(1).to(device)
+#         target = target.to(device)
+#         y = model(data)
+#         loss = F.cross_entropy(y, target)
+#         total_loss += loss.item()
+#         correct += (y.argmax(dim=1) == target).sum().item()
 
 #     print("Test Loss:", total_loss / len(test_loader))
 #     print("Test Accuracy:", 100 * correct / len(test_loader.dataset))
