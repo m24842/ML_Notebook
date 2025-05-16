6fc45330545d867d08df8411d4155dc0a9a02d8a
m24842 <138349218+m24842@users.noreply.github.com>
2025-05-16 15:32:49 -0400

    save

--- src/Packages/m24842_ml/ml/datasets.py
+++ src/Packages/m24842_ml/ml/datasets.py
@@ -319,9 +319,7 @@ class LAMBADA(Dataset):
 
 class ThePile(Dataset):
     def __init__(self, split, tokenizer, min_len=1, max_len=1000, warmup_epochs=0, num_proc=4, root=None):
-        """
-        Efficient Pile loader with optional pre-tokenization.
-        
+        """        
         Args:
             split: one of ["train", "val", "test"]
             tokenizer: tokenizer name or path
