0de02a0dc34f610d4c4b1eccbff1e50a45cdeacf
m24842 <138349218+m24842@users.noreply.github.com>
2025-05-18 17:22:29 -0400

    save

--- src/Packages/m24842_ml/ml/schedulers.py
+++ src/Packages/m24842_ml/ml/schedulers.py
@@ -1,19 +1,29 @@
 import sys
+import math
 from torch.optim.lr_scheduler import *
-from transformers import get_cosine_schedule_with_warmup
+from torch.optim.lr_scheduler import _LRScheduler
 
-class CosineAnnealingLRWithWarmup():
-    def __init__(self, optimizer, num_warmup_steps, num_training_steps, num_cycles=0.5, last_epoch=-1):
-        self.scheduler = get_cosine_schedule_with_warmup(
-            optimizer,
-            num_warmup_steps=num_warmup_steps,
-            num_training_steps=num_training_steps,
-            num_cycles=num_cycles,
-            last_epoch=last_epoch
-        )
+class CosineAnnealingLRWithWarmup(_LRScheduler):
+    def __init__(self, optimizer, num_warmup_steps, num_training_steps, num_cycles=0.5, eta_min=0.0, last_epoch=-1):
+        self.warmup_steps = num_warmup_steps
+        self.num_training_steps = num_training_steps
+        self.num_cycles = num_cycles
+        self.eta_min = eta_min
+        super().__init__(optimizer, last_epoch)
 
-    def __getattr__(self, name):
-        return getattr(self.scheduler, name)
+    def get_lr(self):
+        current_step = self.last_epoch + 1
+        if current_step < self.warmup_steps:
+            return [
+                base_lr * current_step / self.warmup_steps
+                for base_lr in self.base_lrs
+            ]
+        else:
+            progress = (current_step - self.warmup_steps) / (self.num_training_steps - self.warmup_steps)
+            return [
+                self.eta_min + (base_lr - self.eta_min) * 0.5 * (1.0 + math.cos(math.pi * ((self.num_cycles * progress) % 1.0)))
+                for base_lr in self.base_lrs
+            ]
 
 def initialize_scheduler(name, *args, **kwargs):
     scheduler_class = getattr(sys.modules[__name__], name, None)
